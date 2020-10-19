48d63310b4dead164a146348f1138d6b5276c126
romanchukas <romanchukas@nspk.ru>
2020-10-19 16:36:30 +0300

    initial commit

---
+++ src/main/java/com/example/demo/JabelTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+/*
+    @author romanchukas, 19.10.2020
+*/
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JabelTest {
+    public void test(){
+        var tst = "this is var!";
+        System.out.println(tst);
+    }
+}
